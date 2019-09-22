56613cd513196fdac163fb08f51b03a843a7f549
Yu.A.Maykov@yandex.ru <Yu.A.Maykov@yandex.ru>
2019-09-22 02:47:44 -0700

    rename to LoginViewController

--- Vk&Maykov/Vk&Maykov/Source/Controllers/LoginViewController.swift
+++ Vk&Maykov/Vk&Maykov/Source/Controllers/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  Vk&Maykov
 //
 //  Created by Юрий on 15/09/2019.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var titleLabel: UILabel!
